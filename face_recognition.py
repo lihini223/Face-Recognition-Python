f49342b85b22874c76b430d10bd9f0ec95ad4990
Lihini Nisansala <lihininisansala95@gmail.com>
2021-08-02 23:54:54 +0530

    read images

--- face_recognition.py
+++ face_recognition.py
@@ -1,1 +1,13 @@
+import cv2
+
+# Get user inputs
+image_path = sys.argv[1]
+cascade_path = sys.argv[2]
+
+# Initialize the cascade
+face_cascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(image_path)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
