5e9c55804dd564369015ed3f67c6875781e41d3e
lihini223 <lihininisansala95@gmail.com>
2021-08-28 02:35:03 +0530

    Detect faces and draw bounding boxes

--- face_recognition.py
+++ face_recognition.py
@@ -1,21 +1,32 @@
 import cv2
+import sys
 
 # Get user inputs
 image_path = sys.argv[1]
 cascade_path = sys.argv[2]
 
 # Initialize the cascade
-face_cascade = cv2.CascadeClassifier(cascPath)
+face_cascade = cv2.CascadeClassifier(cascade_path)
 
 # Read the image
 image = cv2.imread(image_path)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Detect face
-faces = faceCascade.detectMultiScale(
-  gray,
-  scaleFactor = 1.1,
-  minNeighbors = 5,
-  minSize = (30, 30)
-  flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags=cv2.CASCADE_SCALE_IMAGE
 )
+
+print(f'Found {(len(faces))} faces!')
+
+# Draw a bounding box around the faces
+for (x, y, w, h) in faces:
+  cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imshow("Faces Found", image)
+
+cv2.waitKey(0)
